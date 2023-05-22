3ddfc8926285c001a12ef5384d3c02bc98636024
Nathan <npieraut@live.fr>
2023-05-22 14:27:33 -0400

    nextjs config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
-/** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: process.env.NEXT_PUBLIC_URL,
+  images: {
+    domains: ['ibb.co', 'i.ibb.co', 'example.com'],
+  },
+  serverRuntimeConfig: {
+    auth0Domain: process.env.AUTH0_DOMAIN,
+    auth0ClientId: process.env.AUTH0_CLIENT_ID,
+    auth0RedirectUri: process.env.AUTH0_REDIRECT_URI,
+    auth0Audience: process.env.AUTH0_AUDIENCE,
+    backendUrl: process.env.BACKEND_URL,
+  },
+  publicRuntimeConfig: {
+    // Add any public runtime configs here, if needed
+  }
 }
 
 module.exports = nextConfig
