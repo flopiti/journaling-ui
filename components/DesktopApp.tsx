3ec64ea50e31a1281a9d49b31d1aec048bdf687d
Nathan <npieraut@live.fr>
2023-06-02 15:29:51 -0500

    selecting a note

--- components/DesktopApp.tsx
+++ components/DesktopApp.tsx
@@ -6,14 +6,38 @@ import styles from '@/styles/Home.module.css'
 const DesktopApp = () => {
     const[notes, setNotes] = useState<Note[]>([])
     const {createNote, getNotes, deleteNote } = useNotes();
+    const [selectedNote, setSelectedNote] = useState<number | null>(null);
 
     useEffect(() => {
       getNotes().then((notes) => {
         setNotes(notes)
-        console.log(notes)
       })
-    }, [])
-  
+
+      const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'ArrowUp') {
+          setSelectedNote(prevSelectedNote => 
+            prevSelectedNote !== null && prevSelectedNote > 0
+              ? prevSelectedNote - 1 
+              : prevSelectedNote === null
+                ? notes.length - 1 
+                : null);
+        } else if (event.key === 'ArrowDown') {
+          setSelectedNote(prevSelectedNote => 
+            prevSelectedNote !== null && prevSelectedNote < notes.length - 1 
+              ? prevSelectedNote + 1 
+              : prevSelectedNote === null 
+                ? notes.length - 1 
+                : null
+          );
+          
+                }
+      };
+      window.addEventListener('keydown', handleKeyDown);
+          return () => {
+        window.removeEventListener('keydown', handleKeyDown);
+      };
+    }, [notes.length]);
+
     const [text, setText] = useState('');
   
     const handleSubmit = (event:any) => {
@@ -51,12 +75,12 @@ const DesktopApp = () => {
     return (
         <div className={styles.desktopApp}>
         {
-                notes.map((note) => {
-                console.log(note.datetime);
-                const dateNew: Date = convertUTCDateToLocalDate(new Date(note.datetime));
-                let formattedDate = `${dateNew.getFullYear()}-${String(dateNew.getMonth() + 1).padStart(2, '0')}-${String(dateNew.getDate()).padStart(2, '0')} ${String(dateNew.getHours()).padStart(2, '0')}:${String(dateNew.getMinutes()).padStart(2, '0')}:${String(dateNew.getSeconds()).padStart(2, '0')}`;
-
-                return <div key={note.id} className={styles.note}>
+                notes.map((note, index) => {
+                  const isSelected = index === selectedNote;
+                  const noteClass = isSelected ? `${styles.note} ${styles.selected}` : styles.note;                
+                  const dateNew: Date = convertUTCDateToLocalDate(new Date(note.datetime));
+                  let formattedDate = `${dateNew.getFullYear()}-${String(dateNew.getMonth() + 1).padStart(2, '0')}-${String(dateNew.getDate()).padStart(2, '0')} ${String(dateNew.getHours()).padStart(2, '0')}:${String(dateNew.getMinutes()).padStart(2, '0')}:${String(dateNew.getSeconds()).padStart(2, '0')}`;
+                return <div key={note.id} className={noteClass}>
                     <span className={styles.date}>
                     {formattedDate}    
                     </span>
